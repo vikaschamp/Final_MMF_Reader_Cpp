f6816409671e4733182fcb547bf659b6949e5a69
vikaschamp <vikasjai134@gmail.com>
2020-08-12 19:39:04 +0530

    Shared memory implementation in cpp

---
+++ SharedMemoryReader.cpp
@@ -0,0 +1,295 @@
+#include "SharedMemoryReader.h"
+
+TCHAR szMsg[] = TEXT("Message from first process.");
+SharedMemoryReader::SharedMemoryReader(string name, int pageCount, int pageSize)
+{
+	readCount = 0;
+	this->FileName = name;
+	this->pageCount = pageSize / PADDING;
+	this->pageSize = pageSize;
+	this->mutexName = this->FileName + "mutex";
+
+	thread spawn_thread1(&SharedMemoryReader::BackgroundProcess, this);
+	spawn_thread1.detach();
+
+	GetAllocationGranulityValue();
+
+	/*calculateAllGranulitySize();*/
+
+	OpenSharedMEmoryFile();
+	//OpenExistingNamedMutex();
+	std::cout << "Shared file opend and mutex created" << std::endl;
+	/*Sleep(5000);*/
+	//ReadDataFromSharedMemory();
+	calculateAllGranulitySize();
+}
+
+bool SharedMemoryReader::print()
+{
+	return true;
+}
+
+void SharedMemoryReader::GetAllocationGranulityValue()
+{
+	GetSystemInfo(&SysInfo);
+	GranulityAllocationValue = SysInfo.dwAllocationGranularity;
+}
+
+bool SharedMemoryReader::OpenSharedMEmoryFile()
+{
+	std::wstring sopeFileName = std::wstring(FileName.begin(), FileName.end());
+	LPCWSTR shFileName = sopeFileName.c_str();
+
+	FileHandle = OpenFileMapping(
+		FILE_MAP_ALL_ACCESS,
+		FALSE,
+		shFileName);
+	if (FileHandle == NULL)
+	{
+		std::cout << "Shared file name is not found" << std::endl;
+		return false;
+	}
+	return true;
+}
+
+void SharedMemoryReader::calculateAllGranulitySize()
+{
+	int readCount = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		vector<int> collectionOfvector;
+
+		GranulityLowOffset = (readCount / GranulityAllocationValue) * GranulityAllocationValue;
+		//	collectionOfvector.push_back(GranulityLowOffset);
+
+		GranulityViewSize = (readCount % GranulityAllocationValue) + BYTESIZE;
+		//	collectionOfvector.push_back(GranulityViewSize);
+
+		GranulityVewDelta = readCount - GranulityLowOffset;
+		//collectionOfvector.push_back(GranulityVewDelta);
+
+		granularityCollection[readCount] = GranulityVewDelta/* collectionOfvector*/;
+
+		ReadAllMemoryAddress(readCount, GranulityLowOffset, GranulityViewSize);
+		ReadAllMemoryAddressAndEvent(readCount, GranulityLowOffset, GranulityViewSize, i);
+		readCount += BYTESIZE;
+	}
+	int jamCount = 0;
+	for (auto done : _dictcollectionOfViews)
+	{
+
+		auto bhai = (char*)_dictcollectionOfViews[readCount] + granularityCollection[readCount];
+		jamCount += BYTESIZE;
+	}
+	/*for (auto result : granularityCollection)
+	{
+
+		std::cout << result.first << " ";
+		for (auto vectorResult : result.second)
+		{
+			std::cout << vectorResult << std::endl;
+		}
+		cout << endl;
+
+	}*/
+	/*Sleep(5000);*/
+
+
+	//GranulityLowOffset = (readCount / GranulityAllocationValue) * GranulityAllocationValue;
+	////std::cout << "The file map view starts at %ld bytes into the file.\n" << GranulityLowOffset << endl;
+
+
+	//GranulityViewSize = (readCount % GranulityAllocationValue) + BYTESIZE;
+	////cout << "The file map view is %ld bytes large.\n" << GranulityViewSize << endl;
+
+	//GranulityVewDelta = readCount - GranulityLowOffset;
+	//cout << "The data is  bytes into the view.\n" << GranulityVewDelta << endl;
+}
+
+bool SharedMemoryReader::OpenExistingNamedMutex()
+{
+
+
+
+
+
+	std::wstring scopeNamedMutex = std::wstring(mutexName.begin(), mutexName.end());
+	LPCWSTR shMutex = scopeNamedMutex.c_str();
+
+	namedMutex = OpenMutex(
+		MUTEX_ALL_ACCESS,
+		FALSE,
+		shMutex
+	);
+	if (namedMutex == NULL)
+	{
+		std::cout << "Named mutex is not exist" << std::endl;
+		return false;
+	}
+	return true;
+}
+
+void SharedMemoryReader::ReadAllMemoryAddress(int readCount, DWORD lowOffset, DWORD ViewSize)
+{
+
+
+	_dictcollectionOfViews[readCount] = (void*)MapViewOfFile(FileHandle,
+		FILE_MAP_ALL_ACCESS,
+		0,
+		lowOffset,
+		ViewSize);
+	std::cout << "Address entry for " << readCount << " lowest " << lowOffset << " ViewSize " << ViewSize << endl;
+
+}
+
+void SharedMemoryReader::ReadAllMemoryAddressAndEvent(int readCount, DWORD lowOffset, DWORD ViewSize, int index)
+{
+	std::string newFileName = FileName + "mutex" + std::to_string(index);
+
+	std::wstring sopeFileName = std::wstring(newFileName.begin(), newFileName.end());
+	LPCWSTR shFileName = sopeFileName.c_str();
+	auto open_event = OpenEvent(EVENT_ALL_ACCESS, TRUE, shFileName);
+
+	_dictcollectionOfViews[readCount] = (void*)MapViewOfFile(FileHandle,
+		FILE_MAP_ALL_ACCESS,
+		0,
+		lowOffset,
+		ViewSize);
+	thread spawn_thread(&SharedMemoryReader::SpawnParralerThread, this, open_event, _dictcollectionOfViews[readCount], granularityCollection[readCount], readCount);
+	spawn_thread.detach();
+
+	//auto data1 = std::async(std::launch::async,  &SharedMemoryReader::SpawnParralerThread, this, open_event, _dictcollectionOfViews[readCount], granularityCollection[readCount], readCount);
+
+
+	std::cout << "Address entry for " << readCount << " lowest " << lowOffset << " ViewSize " << ViewSize << " thread id " << endl;
+
+
+}
+
+void SharedMemoryReader::SpawnParralerThread(HANDLE openEvent, void* address, DWORD vieDelta, int readCount)
+{
+
+	while (1)
+	{
+		WaitForSingleObject(
+			openEvent,
+			INFINITE);
+		auto startReading = std::chrono::high_resolution_clock::now();
+		auto output = (char*)address + vieDelta;
+		auto iData = (New_Request*)output;
+		auto stopReading = iData->stopWatchTime = std::chrono::high_resolution_clock::now();
+		_BackgroundQueue.Push(*iData);
+		auto queueFinish = std::chrono::high_resolution_clock::now();
+		auto ReadingElapsed = queueFinish.time_since_epoch().count()-  stopReading.time_since_epoch().count();
+
+
+		/*iData->print();*/
+
+		//auto elapsed = startReading.time_since_epoch().count() - iData->SecurityId;
+		cout << readCount << " queuing elapsed " << ReadingElapsed * 0.001 <<std::endl;
+		//cout << iData->ClOrderID << " " << iData->RequestType << endl;
+	}
+
+}
+
+
+void SharedMemoryReader::ReadDataFromSharedMemory()
+{
+	auto start_calculation = chrono::high_resolution_clock::now();
+	calculateAllGranulitySize();
+	auto stop_Calculation = chrono::high_resolution_clock::now();
+	// std::cout << to_string((stop_Calculation.time_since_epoch().count() - start_calculation.time_since_epoch().count())) << endl;
+
+	int dataCount = 0;
+
+
+
+	//readSharedAddress = (void*)MapViewOfFile(FileHandle,
+	//	FILE_MAP_ALL_ACCESS,
+	//	0,
+	//	GranulityLowOffset,
+	//	GranulityViewSize);
+	for (;;)
+	{
+		WaitForSingleObject(
+			namedMutex,    // handle to mutex
+			INFINITE);
+
+
+
+		//	if (readSharedAddress != NULL)
+		{
+			auto start_reading = chrono::high_resolution_clock::now();
+			auto output = (char*)_dictcollectionOfViews[dataCount] + granularityCollection[dataCount]/*GranulityVewDelta*/;
+
+			/*auto output_new = _collectionOfViews[]*/
+			/*auto iData = (Test*)output;
+			std::cout << "Old " << iData->id << std::endl;*/
+			//	auto time = *(long long int*)output;
+
+			auto iData = (Test*)output;
+
+
+			/*CopyMemory((PVOID)_dictcollectionOfViews[dataCount], szMsg,  (28 * sizeof(TCHAR)));*/
+
+
+			//auto stop_reading = chrono::high_resolution_clock::now();
+			//auto elasped = ((stop_reading.time_since_epoch().count() - time) - 8) * 0.001;
+			//std::cout << "Old " << time << " elpased " << elasped << std::endl;
+
+			/*std::cout << "Calculation : " << elasped << " rcv time " << time << " reading time " << (stop_reading.time_since_epoch().count() - start_reading.time_since_epoch().count()) * 0.001 << " " << (((stop_reading.time_since_epoch().count() - time) * 0.001)) << endl;*/
+
+			//readCount = readCount + BYTESIZE;
+			//if (output[0] != '0' && time != 0)
+			{
+				Test test;
+				test.id = 0;
+
+
+				//const char check[10] = "0";
+				/*	auto ReplaceStart = std::chrono::high_resolution_clock::now();*/
+					// CopyMemory((PVOID)output, &test, (sizeof(Test)));
+					/*auto ReplaceStop = std::chrono::high_resolution_clock::now();*/
+				//CopyMemory((PVOID)output, &check, 10 * (sizeof(char)));
+
+				auto time1 = *(long long int*)output;
+
+				auto iData1 = (Test*)output;
+
+				//std::cout << " new " << output << " " << time << endl;
+				std::cout << "Old " << iData->id << " New " << iData1->id << endl;
+				//const char* data = "N";
+				//CopyMemory((PVOID)_dictcollectionOfViews[dataCount], data, (2*sizeof(char)));
+				////memcpy((PVOID)_dictcollectionOfViews[dataCount], data, (sizeof(char)));
+				//std::cout << output <<" "<<time <<" count "<<dataCount<<" again "<< *(long long int*)output << std::endl;
+				dataCount = dataCount + BYTESIZE;
+
+			}
+			if (dataCount == 1000)
+			{
+				dataCount = 0;
+				std::cout << "Start from Zero" << std::endl;
+			}
+			Sleep(1000);
+
+			/*CreateEventW*/
+			ReleaseMutex(namedMutex);
+		}
+	}
+}
+
+void SharedMemoryReader::BackgroundProcess()
+{
+
+	for (;;)
+	{
+		New_Request request;
+		if (_BackgroundQueue.Pop(request))
+		{
+			//request.print();
+			auto ReadingElapsed = request.stopWatchTime.time_since_epoch().count() - request.ClOrderID;
+			cout << readCount << " reading elapsed " << ReadingElapsed * 0.001 << std::endl;
+		}
+	}
+
+}
---
+++ SharedMemoryReader.h
@@ -0,0 +1,92 @@
+#pragma once
+#ifndef SHAREMEMORY_H
+#define SHAREMEMORY_H
+
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <unordered_map>
+#include <Windows.h>
+#include <thread>
+#include <future>
+#include "BlockingQueue.h"
+using namespace std;
+#define BYTESIZE 120;
+#define PADDING 10;
+
+struct Test
+{
+public:
+	long long int id;
+	char name[10];
+	void clear()
+	{
+		id = 0;
+	}
+};
+
+#pragma pack(1)
+struct New_Request
+{
+public:
+	char RequestType;
+	int64_t ClOrderID;
+	int64_t OriginatorClOrderID;
+	int64_t OrderQuantity;
+	int64_t Price;
+	int8_t OrderType;
+	int8_t OrderSide;
+	int8_t TimeInForce;
+	int8_t SHFEComboOffsetFlag;
+	int8_t SecurityType;
+	char Symbol[10];
+	char SecurityId[25];
+	char ExchangeOrderId[40];
+	std::chrono::steady_clock::time_point stopWatchTime;
+	void print()
+	{
+		std::cout << RequestType << " " << ClOrderID << " " << OriginatorClOrderID << " " << Price << " " << std::to_string(OrderType) << " " << OrderSide << " " << TimeInForce << " " << SHFEComboOffsetFlag << " " << SecurityType << " "
+			<< " " << Symbol << " " << SecurityId << " " << ExchangeOrderId << endl;
+	}
+
+};
+
+
+class SharedMemoryReader
+{
+private:
+	int pageCount;
+	int pageSize;
+	string FileName;
+	string mutexName;
+	DWORD GranulityViewSize;
+	DWORD GranulityLowOffset;
+	DWORD GranulityVewDelta;
+	DWORD  GranulityAllocationValue;
+	DWORD readCount;
+	LPVOID readSharedAddress;
+	bool print();
+	std::unordered_map<int, void*> _dictcollectionOfViews;
+	HANDLE namedMutex;
+	std::unordered_map<int, DWORD> granularityCollection;
+	HANDLE FileHandle;
+	SYSTEM_INFO SysInfo;
+	void GetAllocationGranulityValue();
+	bool OpenSharedMEmoryFile();
+	void calculateAllGranulitySize();
+	bool OpenExistingNamedMutex();
+	void ReadAllMemoryAddress(int, DWORD, DWORD);
+	void ReadAllMemoryAddressAndEvent(int, DWORD, DWORD, int);
+
+	void SpawnParralerThread(HANDLE, void*, DWORD, int);
+
+	BlockingQueue<New_Request> _BackgroundQueue;
+
+
+public:
+	SharedMemoryReader(string, int, int);
+	void ReadDataFromSharedMemory();
+	void BackgroundProcess();
+
+};
+#endif
